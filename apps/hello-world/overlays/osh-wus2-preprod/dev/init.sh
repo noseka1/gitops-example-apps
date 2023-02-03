525f739573d40e73c0ccc9fae73c3a540c6e948e
Ales Nosek <ales.nosek@gmail.com>
2023-02-03 14:16:40 -0800

    Update message

--- apps/hello-world/overlays/osh-wus2-preprod/dev/init.sh
+++ apps/hello-world/overlays/osh-wus2-preprod/dev/init.sh
@@ -3,5 +3,5 @@
 echo Hello world from dev!
 
 while true; do
-  printf 'HTTP/1.1 200 OK\n\n%s' "Hello world from dev!" | nc -l 8080
+  printf 'HTTP/1.1 200 OK\n\nHello world from dev!\n' | nc -l 8080
 done
